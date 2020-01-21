964b8a48141d2b41d48fb3e958267e4e60a5d42f
Alexander Benson <alexander.benson@student.nmt.edu>
2020-01-20 21:31:05 -0700

    Update tokenizer_test.py

--- test/tokenizer_test.py
+++ test/tokenizer_test.py
@@ -1,5 +1,9 @@
-import unittest
-from ../src/run import *
+import unittest, sys
+sys.path.insert(1,'../src/frontend')
+from run import *
+
+
+
 class MyFirstTests(unittest.TestCase):
   def test_hello(self):
     self.assertEqual(hello_world(), 'yello world')
