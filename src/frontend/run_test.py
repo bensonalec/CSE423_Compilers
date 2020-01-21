5d395d30a7c6425a07f8b336adb77ab053bbd92b
Alexander Benson <alexander.benson@student.nmt.edu>
2020-01-20 21:18:47 -0700

    Update run_test.py

--- src/frontend/run_test.py
+++ src/frontend/run_test.py
@@ -2,4 +2,4 @@ import unittest
 from run import *
 class MyFirstTests(unittest.TestCase):
   def test_hello(self):
-    self.assertEqual(hello_world(), 'hello world')
+    self.assertEqual(hello_world(), 'yello world')
