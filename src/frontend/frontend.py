64883d86e2f273e7957f9edd9bd7ff4ffdcc91b6
Alec Benson <alexander.benson@student.nmt.edi>
2020-01-23 10:09:48 -0700

    Added new function to retrieve a string representation of parse tree for testing, and refined the print function

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -5,15 +5,47 @@ from ast import AbstractSyntaxTree
 #function to print the tree
 
 
+def getTree(head,level):
+	level += 1
+	token = head.token
+	content = head.content
+	li = []
+	out = ""
+	for node in content:
+		if(type(node) != type(AbstractSyntaxTree("sample","sample"))):
+			li.append(node)
+		else:
+			li.append(node.token)
+	
+	out = (level,li)
+
+
+	#iterate through the components of the BNF
+	for node in content:
+		if(type(node) == type(AbstractSyntaxTree("sample","sample"))):
+			out += getTree(node,level)
+	return out
+
 def printTree(head,level):
 	level += 1
 	token = head.token
 	content = head.content
-	print("Level: ",level,"Node: ",content, "Type: ",token)
+	li = []
+	out = ""
+	for node in content:
+		if(type(node) != type(AbstractSyntaxTree("sample","sample"))):
+			li.append(node)
+		else:
+			li.append(node.token)
+	
+	print(level,":",li)
+
+
 	#iterate through the components of the BNF
 	for node in content:
 		if(type(node) == type(AbstractSyntaxTree("sample","sample"))):
 			printTree(node,level)
+
 
 #open file for testing
 fi = open("./test_files/parsertest.c","r")
@@ -31,5 +63,5 @@ parser = pg.get_parser()
 parser.parse(tokens)
 head = pg.getTree()
 #print the tree starting at the head
-printTree(head,0)
+(printTree(head,0))
 
