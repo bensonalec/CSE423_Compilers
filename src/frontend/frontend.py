6d6cf2639d26f26ed1a747a85918d9050d1ae538
jdcia <jacob.garcia@student.nmt.edu>
2020-01-23 18:32:17 +0000

    Add command line inputs. Right now it prints usage statments along with taking an option to print tokens. Also it prints tokens and still outputs parser data.

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -1,3 +1,4 @@
+import argparse
 from lexer import *
 from parser import Parser
 from ast import AbstractSyntaxTree
@@ -16,17 +17,49 @@ def printTree(head,level):
 		if(type(node) == type(AbstractSyntaxTree("sample","sample"))):
 			printTree(node,level)
 
+
 #main function to control the frontend with different command line options.
 def main():
-	#open file for testing
-	#fi = open("./test_files/parsertest.c","r")
-	fi = open(sys.argv[1], "r")
+
+	#command line arguement
+
+	#decription of the comiler
+	cmd_options = argparse.ArgumentParser(description='Frontend of the compiler. Can produce tokens and syntax tree')
+	
+	#input file option
+	cmd_options.add_argument('input_file', metavar='<filename.c>', type=str, help='Input c file.')
+	
+	#Arguement to print tokens from lexer
+	cmd_options.add_argument('-l','--lex', help='Prints out tokens from lexer', action='store_true')
+	
+	#Print all output from lexer, parser, etc....
+	cmd_options.add_argument('-a','--all', help='Prints out all intermediate ouputs.', action="store_true")
+
+	args = cmd_options.parse_args()
+
+
+	#open file for testing and make sure file is of the correct type
+	if args.input_file and args.input_file.endswith(".c"):
+		fi = open(args.input_file, "r")
+	else:
+		if not args.input_file.endswith(".c"):
+			print("Error file must end with .c")
+		cmd_options.print_help()
+		exit()
+
+	print(args.lex)
+
 	text_input = fi.read()
 	fi.close()
 
 	#setup lexer, produce tokens
 	lexer = Lexer().get_lexer()
+
 	tokens = lexer.lex(text_input)
+	if args.lex:
+		temp_print = lexer.lex(text_input)
+		for i in temp_print:
+			print(i)
 	#print(tokensToString(tokens))
 	#set up parser, pares the given tokens and retrieve the head of the ast
 	pg = Parser()
