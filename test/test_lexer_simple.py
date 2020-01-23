443d9ef3143cbb732e261c45c5cab540a695c3e3
Keaton Jones <keatonjones44@gmail.com>
2020-01-22 19:20:11 -0700

    added global pathname to test file

--- test/test_lexer_simple.py
+++ test/test_lexer_simple.py
@@ -4,126 +4,128 @@ sys.path.append('../src/frontend')
 import unittest
 from lexer import *
 
+path_to_C_files = "./C_testing_code/regex/"
+path_to_output_files = "./expected_output/regex/"
 
 class SimpleTokenizerTests(unittest.TestCase):
 
     maxDiff = None
 
     def test_Comments(self):
-        fi = open("./C_testing_code/regex/comments.c")
+        fi = open(path_to_C_files + "comments.c")
         text_input = fi.read()
         fi.close()
         lexer = Lexer().get_lexer()
         tokens = lexer.lex(text_input)
-        fi = open("./expected_output/regex/comments")
+        fi = open(path_to_output_files + "comments")
         expected = fi.read()
         fi.close()
 
         self.assertEqual(tokensToString(tokens), expected)
 
     def test_PreProcessor(self):
-        fi = open("./C_testing_code/regex/preProcessor.c")
+        fi = open(path_to_C_files + "preProcessor.c")
         text_input = fi.read()
         fi.close()
         lexer = Lexer().get_lexer()
         tokens = lexer.lex(text_input)
-        fi = open("./expected_output/regex/preProcessor")
+        fi = open(path_to_output_files + "preProcessor")
         expected = fi.read()
         fi.close()
 
         self.assertEqual(tokensToString(tokens), expected)
 
     def test_ImportingLibraries(self):
-        fi = open("./C_testing_code/regex/importingLibraries.c")
+        fi = open(path_to_C_files + "importingLibraries.c")
         text_input = fi.read()
         fi.close()
         lexer = Lexer().get_lexer()
         tokens = lexer.lex(text_input)
-        fi = open("./expected_output/regex/importingLibraries")
+        fi = open(path_to_output_files + "importingLibraries")
         expected = fi.read()
         fi.close()
 
         self.assertEqual(tokensToString(tokens), expected)
 
     def test_Type_Assignment_Hex_Oct_Int(self):
-        fi = open("./C_testing_code/regex/typeAssignHexOctInt.c")
+        fi = open(path_to_C_files + "typeAssignHexOctInt.c")
         text_input = fi.read()
         fi.close()
         lexer = Lexer().get_lexer()
         tokens = lexer.lex(text_input)
-        fi = open("./expected_output/regex/typeAssignHexOctInt")
+        fi = open(path_to_output_files + "typeAssignHexOctInt")
         expected = fi.read()
         fi.close()
 
         self.assertEqual(tokensToString(tokens), expected)
 
     def test_Strings(self):
-        fi = open("./C_testing_code/regex/strings.c")
+        fi = open(path_to_C_files + "strings.c")
         text_input = fi.read()
         fi.close()
         lexer = Lexer().get_lexer()
         tokens = lexer.lex(text_input)
-        fi = open("./expected_output/regex/strings")
+        fi = open(path_to_output_files + "strings")
         expected = fi.read()
         fi.close()
 
         self.assertEqual(tokensToString(tokens), expected)
 
     def test_Keywords(self):
-        fi = open("./C_testing_code/regex/keywords.c")
+        fi = open(path_to_C_files + "keywords.c")
         text_input = fi.read()
         fi.close()
         lexer = Lexer().get_lexer()
         tokens = lexer.lex(text_input)
-        fi = open("./expected_output/regex/keywords")
+        fi = open(path_to_output_files + "keywords")
         expected = fi.read()
         fi.close()
 
         self.assertEqual(tokensToString(tokens), expected)
     
     def test_Bin(self):
-        fi = open("./C_testing_code/regex/bin.c")
+        fi = open(path_to_C_files + "bin.c")
         text_input = fi.read()
         fi.close()
         lexer = Lexer().get_lexer()
         tokens = lexer.lex(text_input)
-        fi = open("./expected_output/regex/bin")
+        fi = open(path_to_output_files + "bin")
         expected = fi.read()
         fi.close()
 
         self.assertEqual(tokensToString(tokens), expected)
     
     def test_Precision(self):
-        fi = open("./C_testing_code/regex/precision.c")
+        fi = open(path_to_C_files + "precision.c")
         text_input = fi.read()
         fi.close()
         lexer = Lexer().get_lexer()
         tokens = lexer.lex(text_input)
-        fi = open("./expected_output/regex/precision")
+        fi = open(path_to_output_files + "precision")
         expected = fi.read()
         fi.close()
 
         self.assertEqual(tokensToString(tokens), expected)
     
     def test_Comparison(self):
-        fi = open("./C_testing_code/regex/comparison.c")
+        fi = open(path_to_C_files + "comparison.c")
         text_input = fi.read()
         fi.close()
         lexer = Lexer().get_lexer()
         tokens = lexer.lex(text_input)
-        fi = open("./expected_output/regex/comparison")
+        fi = open(path_to_output_files + "comparison")
         expected = fi.read()
         fi.close()
 
         self.assertEqual(tokensToString(tokens), expected)
     
     def test_Arithmetic(self):
-        fi = open("./C_testing_code/regex/arithmetic.c")
+        fi = open(path_to_C_files + "arithmetic.c")
         text_input = fi.read()
         fi.close()
         lexer = Lexer().get_lexer()
         tokens = lexer.lex(text_input)
-        fi = open("./expected_output/regex/arithmetic")
+        fi = open(path_to_output_files + "arithmetic")
         expected = fi.read()
         fi.close()
 
