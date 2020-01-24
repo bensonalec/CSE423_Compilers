48353c4781de007ba41a7eb1a51dda5e078861ee
Alec Benson <alexander.benson@student.nmt.edi>
2020-01-24 16:15:55 -0700

    First draft of script that reads in a series of BNF descriptions and outputs a parser.py file

---
+++ bnfToParser.py
@@ -0,0 +1,96 @@
+import re
+
+	
+funcTemp = 	"""
+		@self.pg.production(BNFSPOT)
+		def FUNCNAMESPOT(p):
+			newNode = AbstractSyntaxTree("NAMESPOT",p)
+			return newNode
+"""
+headTemp = 	"""
+		@self.pg.production('BNFSPOT')
+		def program(p):
+			newNode = AbstractSyntaxTree("NAMESPOT",p)
+			self.Head = newNode
+			return newNode
+"""
+
+initTemp = """
+	def __init__(self):
+		self.pg = ParserGenerator(
+			TOKENSPOT
+		)
+		#initialzie head and current node
+		self.Head = None
+"""
+
+fi = open("BNF_definition","r")
+cont = fi.read()
+fi.close()
+reg = r'([A-Z][A-Z|_]*[A-Z])'
+reg = re.compile(reg)
+
+tokens = []
+for group in reg.findall(cont):
+	tokens.append("'" + group + "'")
+tokens = list(dict.fromkeys(tokens))
+tokenList = "["
+jon = ","
+join = jon.join(tokens)
+tokenList += join + "]"
+
+initFunc = initTemp.replace("TOKENSPOT",tokenList)
+
+fi = open("BNF_definition","r")
+cont = fi.readlines()
+fi.close()
+functionList = ""
+
+for line in cont:
+	if(line != "\n"):
+		
+		spl = line.split("#")
+		bnf = spl[0].strip()
+		funcname = bnf.replace(" ","_")
+		funcname = funcname.replace(":","_")
+		name = spl[1].strip()
+		if(name == "program"):
+			newFunc = headTemp
+		else:
+			newFunc = funcTemp
+		newFunc = newFunc.replace("BNFSPOT",bnf)
+		newFunc = newFunc.replace("FUNCNAMESPOT",funcname)
+		newFunc = newFunc.replace("NAMESPOT",name)
+		
+		functionList += newFunc
+
+totalOutput = """
+from rply import ParserGenerator
+from ast import *
+
+#setup parser class
+class Parser():
+
+	INITSPOT
+
+	def parse(self):
+
+		FUNCLISTSPOT
+	
+		@self.pg.error
+		def error_handle(token):
+			return ValueError(token)
+
+	#boilerplate function
+	def get_parser(self):
+		return self.pg.build()
+
+	#retrieve the trees head
+	def getTree(self):
+		return self.Head
+
+"""
+
+totalOutput = totalOutput.replace("INITSPOT",initFunc)
+totalOutput = totalOutput.replace("FUNCLISTSPOT",functionList)
+print(totalOutput)
