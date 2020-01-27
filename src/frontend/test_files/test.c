6050da88bd93b1906e2f5c2e2d5b0fb52eee5791
Ohunter <ohunter@github.com>
2020-01-27 13:43:57 -0700

    Initial attempt to remove ambiguity from BNF Grammar

--- src/frontend/test_files/test.c
+++ src/frontend/test_files/test.c
@@ -1,27 +1,36 @@
 int main() {
-	x=1 + 2 - 3 / 5 * 6 + 99 % 10
-	z = 13
+	int x=1 + 2 - 3 / 5 * 6 + 99 % 10;
+	int z = 13;
+	if(x == 13 && z <= 14)
+		z = 14;
+	x=1 + 2 - 3 / 5 * 6 + 99 % 10;
+	z = 13;
+	
 	if(x == 13 && z <= 14) {
-		z = 14
-	x=1 + 2 - 3 / 5 * 6 + 99 % 10
-	z = 13
-	if(x == 13 && z <= 14) {
-		z = 14
+		z = 14;
 	}
+	goto test;
     //yeet
 	/* skeet */
 	/* skeet
 	skeet2
 	*/
-	A = 2
-	AB2 = 2
-	v = "alec";
-	z = "Hello World";
-	y = "Hello world"; w = "Gotcha; Alec"
-	p = "\""
-	e = 12.123
-	a = 'b'
-	l = true
+	int A = 2;
+	int AB2 = 2;
+	char v[] = "alec";
+	char c[] = "Hello World";
+	char y[] = "Hello world"; char w[] = "Gotcha; Alec";
+	char p[] = "\"";
+	float e = 12.123;
+	char a = 'b';
+	char b = 'z';
+	int g = 2, h, i, j = 8;
+	test: test2: x = A;
 	
+	do
+		g+=2;
+	while (g <= 10);
+	
+
 	return 1;
 }
