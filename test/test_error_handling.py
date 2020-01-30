1ca44f2d0926139149d00439f192347ca029391e
Keaton Jones <keatonjones44@gmail.com>
2020-01-30 11:44:53 -0700

    added error handling unit test..cause why not

---
+++ test/test_error_handling.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append('../src/frontend')
+
+import unittest
+from lexer import *
+from rply.errors import LexingError
+from parser import Parser 
+
+
+path_to_C_files = "./C_testing_code/regex/"
+path_to_output_files = "./expected_output/regex/"
+
+class ErrorHandlingTests(unittest.TestCase):
+
+    maxDiff = None
+
+    def test_lexer_fail(self):
+
+        # Invalid tokens '@' and '#'
+        text_input = """
+        int main() {
+            @
+            return 1;
+            #
+        }
+        """
+
+        lexer = Lexer().get_lexer()
+        tokens = lexer.lex(text_input)
+
+        with self.assertRaises(LexingError): validateTokens(tokens)
+
+    def test_parser_fail(self):
+
+        # Missing semi-colon
+        text_input = """
+        int main() {
+            return 1
+        }
+        """
+
+        lexer = Lexer().get_lexer()
+        tokens = lexer.lex(text_input)
+        validateTokens(tokens)
+        
+        pg = Parser()
+        pg.parse()
+        parser = pg.get_parser()
+
+        with self.assertRaises(AssertionError): parser.parse(tokens)
+
+if __name__ == '__main__':
+	unittest.main()
