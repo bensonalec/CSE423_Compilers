b6501ab3505329a016d8f25a2b56e85cbcd56e1e
ohunter <ohoffs@gmail.com>
2020-02-05 09:06:38 -0700

    Documentation update with auto generated Table of Content

--- src/update_docs.py
+++ src/update_docs.py
@@ -3,6 +3,7 @@ This module is only supposed to generate the documentation for the compiler as a
 """
 
 import os
+import re
 
 if __name__ == "__main__":
     os.system("rm -rf ../docs/*")
@@ -14,12 +15,41 @@ if __name__ == "__main__":
     page = index.read()
     usr = user.read()
 
+    sections = [m.start() for m in re.finditer('<h3>', usr)]
+    toc = []
+    for idx, heading in enumerate(sections):
+        start_point = usr.find("<h3>", heading)
+        toc.append((f"#section-{idx}", usr[start_point + len("<h3>"):usr.find("</h3>", start_point)]))
+        usr = usr.replace("<h3>", f"<h3 id=\"section-{idx}\">", 1)
+
+    usr = usr.replace("<th>", "<th style=\"padding: 10px\">")
+    usr = usr.replace("<td>&#x2713;</td>", "<td style=\"background: rgb(170, 255, 170); text-align: center;\">&#x2713;</td>")
+    usr = usr.replace("<td>&#x2715;</td>", "<td style=\"background: rgb(255, 170, 170); text-align: center;\">&#x2715;</td>")
+    usr = usr.replace("<pre>", "<pre style=\"overflow-x:auto;\">")
+
+    table = """
+<li>
+    <h3>
+        <a href="#header-usermanual">User Manual</a>
+    </h3>
+    <ul>
+"""
+    for section in toc:
+        table += f"""
+        <li>
+            <a href="{section[0]}">{section[1]}</a>
+        </li>
+"""
+    table += """
+    </ul>
+</li>"""
+
+    i = page.index("<ul id=\"index\">")
+    page = page[:i + len("<ul id=\"index\">")] + table + page[i + len("<ul id=\"index\">"):]
+
     i = page.index("<section id=\"section-intro\">")
     page = page[:i + len("<section id=\"section-intro\">")] + usr + page[i + len("<section id=\"section-intro\">"):]
 
-    page = page.replace("<th>", "<th style=\"padding: 10px\">")
-    page = page.replace("<td>&#x2713;</td>", "<td style=\"background: rgb(170, 255, 170); text-align: center;\">&#x2713;</td>")
-    page = page.replace("<td>&#x2715;</td>", "<td style=\"background: rgb(255, 170, 170); text-align: center;\">&#x2715;</td>")
     
     index.seek(0)
     index.write(page)
