5faa3ca67a983052114c2fb302045c9f20d607fd
Keaton Jones <keatonjones44@gmail.com>
2020-02-10 15:11:40 -0700

    Added minimal parser tests that work with our current BNF

---
+++ test/C_testing_code/programs/If_Else.c
@@ -0,0 +1,22 @@
+int main() {
+
+    if (1) {
+
+    } else {
+
+    } else if (0) {
+
+    } else {
+
+    }
+
+    if (1)
+        int a = 0;
+    else
+        int b = 1;
+    else if (0)
+        int c = 2;
+    else
+        int d = 3;
+
+}
