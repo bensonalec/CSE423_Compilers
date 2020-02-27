a02dc38fb2ab6c20541c676b275bb58c9977bdd6
Keaton Jones <keatonjones44@gmail.com>
2020-02-27 07:19:54 -0700

    initial commit to support returns for IR_lv1

--- src/optimizer/IR_Lv1_Builder.py
+++ src/optimizer/IR_Lv1_Builder.py
@@ -6,20 +6,26 @@ class LevelOneIR():
         self.symTable = symTable
 
     def construct(self):
+
         sym = self.symTable
-        print(buildBoilerPlate(sym))
-        # print([(x.type, x.name, [(y.type, y.name) for y in self.symTable.symbols if y.is_param and y.scope.split('/')[1] == x.name]) for x in self.symTable.symbols if x.is_function])
         ntv = self.astHead
+
         varIndex = 0
         lastVarName = "_" + str(varIndex)
         bodyList = []
+
+        # list of all bodies within functions in our C program
         for x in self.astHead.children:
             if x.name == "func":
                 bodyList.append((x.children[1].name,x.children[3]))
-        #now bodylist contains the bodies of each function
 
+        returnDigit = 1234
+        returnVarName = f"D.{returnDigit}"
         for i in bodyList:
-            print(returnLines(i[1],0))
+            for x in returnLines(i[1],0, returnVarName)[0]:
+                print(x)
+            returnDigit += 1
+            returnVarName = f"D.{returnDigit}"
 
 def buildBoilerPlate(symTable):
     namesandparams = []
@@ -40,7 +46,7 @@ def buildBoilerPlate(symTable):
             namesandparams.append((x[0],paramsLi,x[1]))
     return namesandparams
 
-def returnLines(node,lastVarName):
+def returnLines(node,lastVarName, returnVarName):
     lines = []
     for element in node.children:
         try:
@@ -48,60 +54,19 @@ def returnLines(node,lastVarName):
             ind = [splits.index(x) for x in splits if element.name in x]
             ind = ind[0]
             if ind == 0:
-                
-                print("Assignment")
-                #preform a DFS from the element node
-                #upon each 
-                tempDive = element.children[1]
-                ntv = [tempDive]
-                
-                isOp = r'\+|\-|\/|\*'
-                opCheck = re.compile(isOp)
-                opStack = []
-                
-                while ntv != []:
-                
-                    # Grabs the first element which will be the residual left most child
-                    cur = ntv[0]
-                    
-                    opStack.append(cur.name)
-                    
-                    ntv = [x for x in cur.children] + ntv[1:]
-                print(opStack)
-                last = len(opStack)
-                for i in opStack[::-1]:
-                    ind = last- 1
-                    
-                    if(opCheck.match(i)):
-                        v1 = opStack[ind+1]
-                        v2 = opStack[ind+2]
-                        print(v1,i,v2)
-                        lines.append(f"_{lastVarName} = {v1}{i}{v2}")
-                        print(lines)
-                        
-                        # del opStack[ind+1]
-                        # del opStack[ind+2]
-                        # opStack[ind] = f"_{lastVarName}"
-                        opStack = opStack[:ind] + [f"_{lastVarName}"] + opStack[ind+3:]
-                        
-                        print(opStack)
-                        pass
-    
-                    elif(i == "var"):
-                        pass
-                    elif(i == "call"):
-                        #NEED TO CAPTURE PARAMETERS
+                #Assignment
 
-                        lastVarName += 1
-                        # lines.append(f"_{lastVarName} = {cur.children[0].name}()")
-                        
-                        expansion = []
-                        pass
-                    else:
-                        pass
-                        # lines.append(f"_{lastVarName} = _{lastVarName}{cur.name}")
-                    last -= 1
-                element.print_AST()
+                varName = element.children[0].children[0].name
+                
+                # Initialize variable (if needed)
+                if len(element.children[0].children) > 1:
+                    varType = element.children[0].children[0].name
+                    varName = element.children[0].children[1].name
+                    lines.append(f"{varType} {varName};")
+
+                # Breakdown arithmetic nodes
+                lines += breakdownArithmetic(element.children[1], varName)
+
             elif ind == 1:
                 print("For loop")
             elif ind == 2:
@@ -109,8 +74,22 @@ def returnLines(node,lastVarName):
                 ret,lastVarName = returnLines(element,lastVarName)
             elif ind == 3:
                 print("If")
-            elif ind == 4:
-                print("Return")
+            elif ind == 4: 
+                #Return
+
+                # It returns some type of arithmetic, break it down.
+                # TODO: it currently breaks down single values like "return 1;"
+                #       should I keep this? or test if it is "complicated" arithmetic?
+                if len(element.children) > 0:
+                    lines += breakdownArithmetic(element.children[0], f"_{lastVarName}")
+                    lines.append(f"{returnVarName} = _{lastVarName}")
+                    lastVarName += 1
+                    lines.append(f"return {returnVarName};") 
+                
+                # Returns nothing
+                else:
+                    lines.append(f"return;") 
+
             elif ind == 5:
                 print("Call")
             elif ind == 6:
@@ -125,7 +104,53 @@ def returnLines(node,lastVarName):
                 print("Label")
             else:
                 print("Unsupported at this time")
-        except Exception:
+
+        except Exception as err:
+            print("Exception: ", err)
             pass
     
     return lines,lastVarName
+
+def breakdownArithmetic(root, varName):
+    ntv = [root]
+    
+    isOp = r'\+|\-|\/|\*'
+    opCheck = re.compile(isOp)
+    opStack = []
+
+    lines = []
+    
+    while ntv != []:
+        cur = ntv[0]
+        opStack.append(cur.name)
+        ntv = [x for x in cur.children] + ntv[1:]
+
+    last = len(opStack)
+    for i in opStack[::-1]:
+        ind = last- 1
+        
+        if(opCheck.match(i)):
+            v1 = opStack[ind+1]
+            v2 = opStack[ind+2]
+
+            lines.append(f"{varName} = {v1}{i}{v2}")
+            opStack = opStack[:ind] + [f"{varName}"] + opStack[ind+3:]
+            pass
+
+        elif(i == "var"):
+            pass
+        elif(i == "call"):
+            #TODO: NEED TO CAPTURE PARAMETERS
+            varName += 1
+            expansion = []
+            pass
+
+        #There is only one value.
+        #I think this will only happen when numbers are passed in and not operators? 
+        #maybe it will happen when function calls are passed in?
+        elif(len(opStack) == 1):
+            lines.append(f"{varName} = {i}")
+
+        last -= 1
+
+    return lines
