9b861aeb97a85be4aa7b304f73932b047a8bb01e
ohunter <ohoffs@gmail.com>
2020-02-26 21:08:44 -0700

    Fixed doc generation for the named tuple in semantics

--- src/frontend/semantics.py
+++ src/frontend/semantics.py
@@ -17,7 +17,7 @@ class Entry():
 
 
 Node = namedtuple("Node", ["Node", "Scope"])
-Node.__docstring__ = """
+Node.__doc__ = """
 A simple namedtuple to allow for better readability when performing the depth first search required for the semantic analysis.
 """
 class symbol_table():
