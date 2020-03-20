7bf1dc7b5486a93f1924f2790088c53345e8f5ce
jdcia <jacob.garcia@student.nmt.edu>
2020-03-20 17:18:44 +0000

    Added some basic IR checking to import_ir.

--- src/optimizer/import_ir.py
+++ src/optimizer/import_ir.py
@@ -4,7 +4,33 @@ class import_ir():
 
         self.lines = [x.strip() for x in fd.readlines()]
 
+        fd.close()
+
 
     def verify(self):
+
+        vars = []
+        labels = []
+        gotos = []
+
+        #checks for undefined variables.
+        for i in self.lines:
+            if i.startswith("int"): #make this more robust.
+                vars.append(i.split(" ")[1][:-1])
+            elif "=" in i and not i.startswith("_") and not i.startswith("D."):
+                if [x for x in vars if i.split("=")[0].strip() == x.strip()] == []:
+                    print("ERROR UNDEFINED VARIABLE")
+                    print(i)
+            elif i.endswith(":"):
+                labels.append(i[:-1])
+            elif i.startswith("goto"):
+                gotos.append(i.split(" ")[1][:-1])
+
+        #check labels
+        for i in gotos:
+            if not i in labels:
+                print("Invalid Goto")
+                print(i)
+
 
         return self.lines
