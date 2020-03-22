62e27167a842c44d4f2b4cdc0ab6e42f9cf9a6cd
ohunter <ohoffs@gmail.com>
2020-03-21 20:47:51 -0600

    removed trailing whitespace from files

--- test/programs/Goto.c
+++ test/programs/Goto.c
@@ -4,5 +4,5 @@ int main() {
 
     int random_var = 0;
 
-    label: 
+    label:
 }
--- test/programs/Initialization_Strings.c
+++ test/programs/Initialization_Strings.c
@@ -1,7 +1,7 @@
 int main() {
 
     char string[] = "test string";
-    
+
     char string_two[] = 'test string';
 
     char string_special[] = "\n\t\\\'\"\%\$%&*$%&%*$&@@   __  +=-";
