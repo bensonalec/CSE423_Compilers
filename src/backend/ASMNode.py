0e03607ae1f20488ce740b819590d43e5ea10c8d
Alec <alecbenson21@gmail.com>
2020-04-10 17:19:20 -0600

    Initial commit of class prototype

---
+++ src/backend/ASMNode.py
@@ -0,0 +1,13 @@
+#this file will solve all our problems. indeed. 
+class ASMNode():
+    def __init__(self, command, left, right):
+        self.command = command
+        self.left = left
+        self.right = right
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
