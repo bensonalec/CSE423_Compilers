60de478c436620b6f561c15752a2d0d2403e13c0
Alec <alecbenson21@gmail.com>
2020-04-10 17:32:58 -0600

    Adding 'final' implementation of ASMNode

--- src/backend/ASMNode.py
+++ src/backend/ASMNode.py
@@ -1,13 +1,17 @@
-#this file will solve all our problems. indeed. 
+#this file will solve all our problems. indeed.
 class ASMNode():
     def __init__(self, command, left, right):
         self.command = command
         self.left = left
         self.right = right
-        pass
 
     def __str__(self):
-        pass
+        if right:
+            return f"{self.command} {self.left}, {self.right};"
+        elif left:
+            return f"{self.command} {self.left};"
+        else:
+            return f"{self.command};"
 
     def __repr__(self):
         pass
