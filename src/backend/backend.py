b5657207c7900d3d9873c38cfab97fbc7481ed6f
Alec <alecbenson21@gmail.com>
2020-04-10 17:42:40 -0600

    Whole group got backend boilerplate working

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -1,7 +1,12 @@
-import importlib
+from inspect import getsourcefile
+from importlib.machinery import SourceFileLoader
+import os
+
+asmn = SourceFileLoader("ASMNode", f"{os.path.dirname(os.path.abspath(getsourcefile(lambda:0)))}/ASMNode.py").load_module()
+ir1 = SourceFileLoader("IR_Lv1_Builder", f"{os.path.dirname(os.path.abspath(getsourcefile(lambda:0)))}/../optimizer/IR_Lv1_Builder.py").load_module()
 
 def main( args, IR):
-	asm = [y.asm() for x in IR for y in x.treelist]
+	asm = [y.asm() for x in IR.IR for y in x.treeList]
 	
 	if args.a1:
 		for i in asm:
